5432f57604095890b18beafbb9606f94f9900708
Eruizmagliano <eruizmagliano@gmail.com>
2022-04-23 17:29:09 -0300

    Agrego criterio validador por top 10000 peores contraseñas

---
+++ src/test/java/TestValidador/TestValidacionCompararContraPeores.java
@@ -0,0 +1,30 @@
+package TestValidador;
+import Validador.CriterioValidador;
+import Validador.ValidacionCompararContraPeores;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestValidacionCompararContraPeores {
+    CriterioValidador validacionCompararContraPeores = new Validador.ValidacionCompararContraPeores();
+
+    @Before
+    public void inicializacion(){
+        validacionCompararContraPeores = new ValidacionCompararContraPeores();
+    }
+    @Test
+    public void contraseniaValida() throws FileNotFoundException {
+        inicializacion();
+        assertTrue(validacionCompararContraPeores.esValida("jose12345"));
+    }
+
+    @Test
+    public void contraseniaInvalida() throws FileNotFoundException {
+        inicializacion();
+        assertFalse(validacionCompararContraPeores.esValida("brady"));
+    }
+}
