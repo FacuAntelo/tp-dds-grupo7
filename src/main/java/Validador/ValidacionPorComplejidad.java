7dce2425372d813ad0ac4b4dd00aa91ba86e21b3
FacuAntelo <fantelo@frba.utn.edu.ar>
2022-04-24 19:16:43 -0300

    Agrego Validador Por Complejidad

--- src/main/java/Validador/ValidacionPorComplejidad.java
+++ src/main/java/Validador/ValidacionPorComplejidad.java
@@ -1,11 +1,23 @@
 package Validador;
 
+import static java.lang.Character.isDigit;
+import static java.lang.Character.isLowerCase;
+
 public class ValidacionPorComplejidad extends CriterioValidador{
     public ValidacionPorComplejidad() {
     }
 
     @Override
     public boolean esValida(String clave) {
-        return false;
+
+        boolean estadoDeValidacion = false;
+
+        try {
+            estadoDeValidacion = clave.chars().anyMatch(Character::isLowerCase) &&
+                                 clave.chars().anyMatch(Character::isDigit);
+        }catch (Exception exception){
+            System.out.println("No se pudo validar por complejidad");
+        }
+        return estadoDeValidacion;
     }
 }
