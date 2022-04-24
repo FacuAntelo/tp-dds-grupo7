5a5a01f45bf42a1060950d994724c8f838325be9
FacuAntelo <fantelo@frba.utn.edu.ar>
2022-04-24 19:10:19 -0300

    Agrego Validador Por Complejidad

---
+++ src/test/java/TestValidador/TestValidacionPorComplejidad.java
@@ -0,0 +1,43 @@
+package TestValidador;
+
+import Validador.CriterioValidador;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestValidacionPorComplejidad {
+
+        CriterioValidador validacionPorLongitud = new Validador.ValidacionPorComplejidad();
+
+        @Before
+        public void inicializacion(){
+            validacionPorLongitud = new Validador.ValidacionPorComplejidad();
+        }
+        @Test
+        public void contieneMinuscula() throws FileNotFoundException {
+            inicializacion();
+            assertTrue(validacionPorLongitud.esValida("CAMAq12123123"));
+        }
+
+        @Test
+        public void noContieneMinuscula() throws FileNotFoundException {
+            inicializacion();
+            assertFalse(validacionPorLongitud.esValida("CAMA12123123"));
+        }
+        @Test
+        public void contieneNumero() throws FileNotFoundException {
+            inicializacion();
+            assertTrue(validacionPorLongitud.esValida("CAMAq12123123"));
+        }
+
+        @Test
+        public void noContieneNumero() throws FileNotFoundException {
+            inicializacion();
+            assertFalse(validacionPorLongitud.esValida("CAMAasdasd$"));
+        }
+    }
+
