8f3d1812f6b38970f3ceab4db79073ef58502845
Jose Martinez <martinez022jose@gmail.com>
2022-04-27 19:44:05 -0300

    Modificacion de variable

--- src/test/java/TestValidador/TestValidacionPorComplejidad.java
+++ src/test/java/TestValidador/TestValidacionPorComplejidad.java
@@ -11,44 +11,44 @@ import static org.junit.Assert.assertTrue;
 
 public class TestValidacionPorComplejidad {
 
-        CriterioValidador validacionPorLongitud = new Validador.ValidacionPorComplejidad();
+        CriterioValidador validacionPorComplejidad = new Validador.ValidacionPorComplejidad();
 
         @Before
         public void inicializacion(){
-            validacionPorLongitud = new Validador.ValidacionPorComplejidad();
+            validacionPorComplejidad = new Validador.ValidacionPorComplejidad();
         }
         @Test
         public void contieneMinuscula() throws FileNotFoundException {
             inicializacion();
-            assertTrue(validacionPorLongitud.esValida("CAMAq12123123"));
+            assertTrue(validacionPorComplejidad.esValida("CAMAq12123123"));
         }
 
         @Test
         public void noContieneMinuscula() throws FileNotFoundException {
             inicializacion();
-            assertFalse(validacionPorLongitud.esValida("CAMA12123123"));
+            assertFalse(validacionPorComplejidad.esValida("CAMA12123123"));
         }
         @Test
         public void contieneNumero() throws FileNotFoundException {
             inicializacion();
-            assertTrue(validacionPorLongitud.esValida("CAMAq12123123"));
+            assertTrue(validacionPorComplejidad.esValida("CAMAq12123123"));
         }
 
         @Test
         public void noContieneNumero() throws FileNotFoundException {
             inicializacion();
-            assertFalse(validacionPorLongitud.esValida("CAMAasdasd$"));
+            assertFalse(validacionPorComplejidad.esValida("CAMAasdasd$"));
         }
         @Test
         public void contieneMayuscula() throws FileNotFoundException {
             inicializacion();
-            assertTrue(validacionPorLongitud.esValida("CAMAq12123123"));
+            assertTrue(validacionPorComplejidad.esValida("CAMAq12123123"));
         }
 
         @Test
         public void noContieneMayuscula() throws FileNotFoundException {
             inicializacion();
-            assertFalse(validacionPorLongitud.esValida("123asdasd$"));
+            assertFalse(validacionPorComplejidad.esValida("123asdasd$"));
         }
     }
 
