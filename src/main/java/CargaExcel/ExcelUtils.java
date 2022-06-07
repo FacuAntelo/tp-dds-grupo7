345fc5dbc6b12af9f3c163d5a5f389d6b7778b95
FacuAntelo <fantelo@frba.utn.edu.ar>
2022-06-06 22:55:50 -0300

    Mock Carga de Excel

---
+++ src/main/java/CargaExcel/ExcelUtils.java
@@ -0,0 +1,59 @@
+package CargaExcel;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.util.Iterator;
+//falta importar mas, agregar dependencias??
+public class ExcelUtils {
+
+        public static void leerExcel(Path path)
+        {
+            try
+            {
+                FileInputStream file = new FileInputStream(new File(path));
+
+                //Crea la instancia de Workbook con referencia a al archivo .xlsx
+                XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+                //Toma la hoja de calculo deseada desde el principio
+                XSSFSheet sheet = workbook.getSheetAt(0);
+
+                //Itera fila por fila una por una
+                Iterator<Row> rowIterator = sheet.iterator();
+                while (rowIterator.hasNext())
+                {
+                    Row row = rowIterator.next();
+                    //Para cada fila itero todas las columnas
+                    Iterator<Cell> cellIterator = row.cellIterator();
+
+                    while (cellIterator.hasNext())
+                    {
+                        Cell cell = cellIterator.next();
+                        //Chequea la celda y el formato
+                        switch (cell.getCellType())
+                        {
+                            case Cell.CELL_TYPE_NUMERIC:
+                                System.out.print(cell.getNumericCellValue() + "t");
+                                break;
+                            case Cell.CELL_TYPE_STRING:
+                                System.out.print(cell.getStringCellValue() + "t");
+                                break;
+                        }
+                    }
+                    System.out.println("");
+                }
+                file.close();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+}
+
+
---
+++ src/main/java/Usuarios/Organizacion.java
@@ -0,0 +1,19 @@
+package Usuarios;
+
+import CargaExcel.ExcelUtils;
+
+import java.nio.file.Path;
+import java.time.LocalDate;
+
+public class Organizacion {
+
+    public String actividad;
+    public String tipoDeConsumo;
+    public String valor;
+    public String periodicidad;
+    public LocalDate localDate;
+
+    public void cargarExcel(Path path) {
+        ExcelUtils.leerExcel(path);
+    }
+
